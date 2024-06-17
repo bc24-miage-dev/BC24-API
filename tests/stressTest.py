40ba454e97c136457b14e515e2478d062efa158e
Etienne Baumgartner <baume96@gmail.com>
2024-06-17 19:58:38 +0200

    almost working for change of metaData

--- tests/stressTest.py
+++ tests/stressTest.py
@@ -1,11 +1,9 @@
 from locust import HttpUser, task, between, events
-import logging
 import random
 
 
 class StressTest(HttpUser):
-    wait_time = between(1, 4)  # Wait between 1 and 2 seconds between tasks
-
+    wait_time = between(1, 4)
     wallet_addresses = [
         "0xFE3B557E8Fb62b89F4916B721be55cEb828dBd73",
         "0x2DFc6e58d8a388cE38b5413ca2458a7b59d1B844",
@@ -26,10 +24,10 @@ class StressTest(HttpUser):
     def get_resource_by_wallet(self):
 
         wallet_address = random.choice(self.wallet_addresses)
-        # Example query parameter, adjust as necessary
+
         metaData = random.choice([True, False])
-        # Example query parameter, adjust as necessary
         recursive = random.choice([True, False])
+
         self.client.get(
             f"/resource/{wallet_address}?metaData={metaData}&recursive={recursive}", name="/resource/[wallet_address]")
 
@@ -38,8 +36,10 @@ class StressTest(HttpUser):
         # only breeder for now
         payload = {
             "wallet_address": "0x2DFc6e58d8a388cE38b5413ca2458a7b59d1B844",
-            "token_id": 1,
-            "metadata": {"key": "value"}
+            "resourceId": 1,
+            "quantity": 1,
+            "metaData": {"key": "value"},
+            "ingredients": []
         }
         headers = {'Content-Type': 'application/json'}
         self.client.post("/resource/mint", json=payload, headers=headers)
@@ -49,17 +49,43 @@ class StressTest(HttpUser):
         payload = {
             "from_wallet_address": "0x2DFc6e58d8a388cE38b5413ca2458a7b59d1B844",
             "resourceId": 1,
-            "metadata": {"key": "value"},
+            "quantity": 1,
+            "metaData": {"key": "value"},
             "ingredients": []
         }
         headers = {'Content-Type': 'application/json'}
-        self.client.post("/resource/mint", json=payload, headers=headers)
+        response = self.client.post(
+            "/resource/mint", json=payload, headers=headers)
 
         payload = {
-            "wallet_address": "0x2DFc6e58d8a388cE38b5413ca2458a7b59d1B844",
-            "token_id": 1,
-            "metadata": {"newKey": "newValue"}
+            "from_wallet_address": "0x2DFc6e58d8a388cE38b5413ca2458a7b59d1B844",
+            "tokenId": response.json()["tokenId"],
+            "metaData": {"newKey": "newValue"}
         }
 
         headers = {'Content-Type': 'application/json'}
         self.client.post("/resource/metadata", json=payload, headers=headers)
+
+    @task
+    def mint_and_transfer_resource(self):
+        # only breeder for now
+        payload = {
+            "from_wallet_address": "0x2DFc6e58d8a388cE38b5413ca2458a7b59d1B844",
+            "resourceId": 1,
+            "quantity": 1,
+            "metaData": {"key": "value"},
+            "ingredients": []
+        }
+        headers = {'Content-Type': 'application/json'}
+        response = self.client.post(
+            "/resource/mint", json=payload, headers=headers)
+
+        payload = {
+            "tokenId": response.json()["tokenId"],
+            "quantity": 1,
+            "from_wallet_address": "0x2DFc6e58d8a388cE38b5413ca2458a7b59d1B844",
+            "to_wallet_address": "0x9F6C344071C0FDf43132eEfA8309a770A063D82D"
+        }
+
+        headers = {'Content-Type': 'application/json'}
+        self.client.post("/resource/transfer", json=payload, headers=headers)
