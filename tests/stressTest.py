6eb8306392174278e9e205fdddbdfb4c935b8c0f
Etienne Baumgartner <baume96@gmail.com>
2024-06-23 20:00:36 +0200

    finishing stress testing

--- tests/stressTest.py
+++ tests/stressTest.py
@@ -1,32 +1,74 @@
 from locust import HttpUser, task, between, events
 import random
 
+from threading import Lock
+
+
+class SafeTokenDict:
+    def __init__(self):
+        self.token_dict = {
+            "sheep_token": [],
+            "cow_token": [],
+            "sheep_carcass_token": [],
+            "cow_carcass_token": [],
+            "sheep_demi_carcass_token": [],
+            "cow_demi_carcass_token": [],
+            "meat_token": [],
+        }
+        self.lock = Lock()
+
+    def append_token(self, token_type, token_id):
+        with self.lock:
+            if token_type in self.token_dict:
+                self.token_dict[token_type].append(token_id)
+                print(f"Appended {token_id} to {token_type}")  # For debugging
+            else:
+                print(f"Token type {token_type} not found")
+
+    def __getitem__(self, key):
+        with self.lock:
+            return self.token_dict[key]
+
 
 class StressTest(HttpUser):
     wait_time = between(1, 4)
-    wallet_addresses = [
-        "0xFE3B557E8Fb62b89F4916B721be55cEb828dBd73",
-        "0x2DFc6e58d8a388cE38b5413ca2458a7b59d1B844",
-        "0x9F6C344071C0FDf43132eEfA8309a770A063D82D",
-        "0x0b97F7B3FC38bF1DFf740d65B582c61b3E84FfC6",
-    ]
+    token_dict = SafeTokenDict()
+
+    admin_wallet_address = "0xFE3B557E8Fb62b89F4916B721be55cEb828dBd73"
+
+    wallet_addresses = {
+        "BREEDER": "0x2DFc6e58d8a388cE38b5413ca2458a7b59d1B844",
+        "SLAUGHTERER": "0x9F6C344071C0FDf43132eEfA8309a770A063D82D",
+        "MANUFACTURER": "0x0b97F7B3FC38bF1DFf740d65B582c61b3E84FfC6",
+    }
 
     def on_start(self):
-        ROLES = ["BREEDER", "SLAUGHTERER", "MANUFACTURER"]
-        for i, wallet_address in enumerate(StressTest.wallet_addresses[1:]):
-            role = ROLES[i % len(ROLES)]  # Cycle through ROLES if not enough
+        for role, wallet_address in self.wallet_addresses.items():
             data = {
-                "from_wallet_address": "0xFE3B557E8Fb62b89F4916B721be55cEb828dBd73",
+                "from_wallet_address": self.admin_wallet_address,
                 "target_wallet_address": wallet_address,
                 "role": role,
             }
             headers = {"Content-Type": "application/json"}
-            self.client.post("/roles/assignRole", json=data, headers=headers)   
-
+            self.client.post("/roles/assignRole", json=data, headers=headers)
 
     @task
     def get_metadata(self):
-        tokenId = random.randint(1, 100)
+        print("Getting metaData")
+        print(self.token_dict.token_dict)
+        all_tokens = (
+            self.token_dict["sheep_token"]
+            + self.token_dict["cow_token"]
+            + self.token_dict["sheep_carcass_token"]
+            + self.token_dict["cow_carcass_token"]
+            + self.token_dict["sheep_demi_carcass_token"]
+            + self.token_dict["cow_demi_carcass_token"]
+            + self.token_dict["meat_token"]
+        )
+
+        if len(all_tokens) == 0:
+            return
+        tokenId = random.choice(all_tokens)
         self.client.get(
             f"/resource/{tokenId}/metadata?recursive=true",
             name="/resource/[tokenId]/metadata",
@@ -34,8 +76,8 @@ class StressTest(HttpUser):
 
     @task
     def get_resource_by_wallet(self):
-
-        wallet_address = random.choice(self.wallet_addresses)
+        print("Getting resource by wallet")
+        wallet_address = random.choice(list(self.wallet_addresses.values()))
 
         metaData = random.choice([True, False])
         recursive = random.choice([True, False])
@@ -46,142 +88,270 @@ class StressTest(HttpUser):
         )
 
     @task
-    def only_mint_resource(self):
-        # only breeder for now
+    def set_meta_data(self):
+        print("Setting metaData")
+        all_tokens = (
+            self.token_dict["sheep_token"]
+            + self.token_dict["cow_token"]
+            + self.token_dict["sheep_carcass_token"]
+            + self.token_dict["cow_carcass_token"]
+            + self.token_dict["sheep_demi_carcass_token"]
+            + self.token_dict["cow_demi_carcass_token"]
+            + self.token_dict["meat_token"]
+        )
+
+        if len(all_tokens) == 0:
+            return
+        tokenId = random.choice(all_tokens)
         payload = {
-            "wallet_address": "0x2DFc6e58d8a388cE38b5413ca2458a7b59d1B844",
-            "resourceId": 45,
-            "quantity": 1,
-            "metaData": {"key": "value"},
-            "ingredients": [],
+            "from_wallet_address": self.admin_wallet_address,
+            "tokenId": tokenId,
+            "metaData": {
+                "key": "value",
+                "key2": "value2",
+                "key3": {"key": "value", "key2": "value2", "key3": "value3"},
+                "key4": "asfasfdasfasfdasfdasfdasdfasdfasdfasfdsadfsadfasfdasdfasfdasfsfasfdasfd",
+                "key5": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            },
         }
+
         headers = {"Content-Type": "application/json"}
-        self.client.post("/resource/mint", json=payload, headers=headers)
+        self.client.post("/resource/metaData", json=payload, headers=headers)
 
     @task
-    def mint_and_change_resource(self):
+    def mint_sheep_or_cow(self):
+        print("Minting sheep or cow")
+
         payload = {
-            "from_wallet_address": "0x2DFc6e58d8a388cE38b5413ca2458a7b59d1B844",
-            "resourceId": 45,
+            "from_wallet_address": self.wallet_addresses["BREEDER"],
+            "resourceId": 42,
             "quantity": 1,
             "metaData": {"key": "value"},
             "ingredients": [],
         }
+
         headers = {"Content-Type": "application/json"}
         response = self.client.post("/resource/mint", json=payload, headers=headers)
+        token_id = response.json()["tokenId"]
+        self.token_dict.append_token("sheep_token", token_id)
 
         payload = {
-            "from_wallet_address": "0x2DFc6e58d8a388cE38b5413ca2458a7b59d1B844",
-            "tokenId": response.json()["tokenId"],
-            "metaData": {"newKey": "newValue"},
-        }
-
-        headers = {"Content-Type": "application/json"}
-        self.client.post("/resource/metadata", json=payload, headers=headers)
-
-    @task
-    def mint_and_transfer_resource(self):
-        # only breeder for now
-        payload = {
-            "from_wallet_address": "0x2DFc6e58d8a388cE38b5413ca2458a7b59d1B844",
-            "resourceId": 45,
+            "from_wallet_address": self.wallet_addresses["BREEDER"],
+            "resourceId": 43,
             "quantity": 1,
             "metaData": {"key": "value"},
             "ingredients": [],
         }
+
         headers = {"Content-Type": "application/json"}
         response = self.client.post("/resource/mint", json=payload, headers=headers)
+        token_id = response.json()["tokenId"]
+        self.token_dict.append_token("cow_token", token_id)
 
-        payload = {
-            "tokenId": response.json()["tokenId"],
-            "quantity": 1,
-            "from_wallet_address": "0x2DFc6e58d8a388cE38b5413ca2458a7b59d1B844",
-            "to_wallet_address": "0x9F6C344071C0FDf43132eEfA8309a770A063D82D",
-        }
+        print(self.token_dict.token_dict)
 
-        headers = {"Content-Type": "application/json"}
-        self.client.post("/resource/transfer", json=payload, headers=headers)
-
-    @task
-    def mint_and_transfer_and_create_new_resource(self):
-        # only breeder for now
-        payload = {
-            "from_wallet_address": "0x2DFc6e58d8a388cE38b5413ca2458a7b59d1B844",
-            "resourceId": 45,
-            "quantity": 1,
-            "metaData": {"key": "value"},
-            "ingredients": [],
-        }
-        headers = {"Content-Type": "application/json"}
-        response = self.client.post("/resource/mint", json=payload, headers=headers)
-
-        payload = {
-                "tokenId": response.json()["tokenId"],
+        if (
+            len(self.token_dict["sheep_token"]) > 0
+            and len(self.token_dict["cow_token"]) > 0
+        ):
+            # transfer sheep
+            payload = {
+                "tokenId": self.token_dict["sheep_token"][-1],
                 "quantity": 1,
-                "from_wallet_address": "0x2DFc6e58d8a388cE38b5413ca2458a7b59d1B844",
-                "to_wallet_address": "0x9F6C344071C0FDf43132eEfA8309a770A063D82D",
+                "from_wallet_address": self.wallet_addresses["BREEDER"],
+                "to_wallet_address": self.wallet_addresses["SLAUGHTERER"],
             }
 
-        headers = {"Content-Type": "application/json"}
-        self.client.post("/resource/transfer", json=payload, headers=headers)
+            headers = {"Content-Type": "application/json"}
+            response = self.client.post(
+                "/resource/transfer", json=payload, headers=headers
+            )
 
-        
-        payload = {
-            "from_wallet_address": "0x9F6C344071C0FDf43132eEfA8309a770A063D82D",
-            "resourceId": 48,
-            "quantity": 1,
-            "metaData": {"key": "value"},
-            "ingredients": [response.json()["tokenId"]],
-        }
+            # transfer cow
+            payload = {
+                "tokenId": self.token_dict["cow_token"][-1],
+                "quantity": 1,
+                "from_wallet_address": self.wallet_addresses["BREEDER"],
+                "to_wallet_address": self.wallet_addresses["SLAUGHTERER"],
+            }
 
-        response = self.client.post("/resource/mint", json=payload, headers=headers)
+            headers = {"Content-Type": "application/json"}
+            response = self.client.post(
+                "/resource/transfer", json=payload, headers=headers
+            )
 
-    
     @task
-    def mint_and_transfer_and_create_new_resource_and_mint_to_many(self):
-        # only breeder for now
-        payload = {
-            "from_wallet_address": "0x2DFc6e58d8a388cE38b5413ca2458a7b59d1B844",
-            "resourceId": 45,
-            "quantity": 1,
-            "metaData": {"key": "value"},
-            "ingredients": [],
-        }
-        headers = {"Content-Type": "application/json"}
-        response = self.client.post("/resource/mint", json=payload, headers=headers)
-
-        payload = {
-                "tokenId": response.json()["tokenId"],
+    def create_carcass(self):
+        print("Creating carcass")
+        if (
+            len(self.token_dict["sheep_token"]) > 0
+            and len(self.token_dict["cow_token"]) > 0
+        ):
+            # slaughter sheep
+            payload = {
+                "from_wallet_address": self.wallet_addresses["SLAUGHTERER"],
+                "resourceId": 45,
                 "quantity": 1,
-                "from_wallet_address": "0x2DFc6e58d8a388cE38b5413ca2458a7b59d1B844",
-                "to_wallet_address": "0x9F6C344071C0FDf43132eEfA8309a770A063D82D",
+                "metaData": {"key": "value"},
+                "ingredients": [self.token_dict["sheep_token"].pop()],
             }
 
-        headers = {"Content-Type": "application/json"}
-        self.client.post("/resource/transfer", json=payload, headers=headers)
+            headers = {"Content-Type": "application/json"}
+            response = self.client.post("/resource/mint", json=payload, headers=headers)
+            self.token_dict.append_token(
+                "sheep_carcass_token", response.json()["tokenId"]
+            )
 
-        payload = {
-            "from_wallet_address": "0x9F6C344071C0FDf43132eEfA8309a770A063D82D",
-            "resourceId": 48,
-            "quantity": 1,
-            "metaData": {"key": "value"},
-            "ingredients": [response.json()["tokenId"]],
-        }
-
-        headers = {"Content-Type": "application/json"}
-        response = self.client.post("/resource/mint", json=payload, headers=headers)
-
-        payload = {
-                "tokenId": response.json()["tokenId"],
+            # slaughter cow
+            payload = {
+                "from_wallet_address": self.wallet_addresses["SLAUGHTERER"],
+                "resourceId": 46,
                 "quantity": 1,
-                "from_wallet_address": "0x9F6C344071C0FDf43132eEfA8309a770A063D82D",
-                "to_wallet_address": "0x0b97F7B3FC38bF1DFf740d65B582c61b3E84FfC6",
+                "metaData": {"key": "value"},
+                "ingredients": [self.token_dict["cow_token"].pop()],
             }
 
-        self.client.post("/resource/transfer", json=payload, headers=headers)
+            headers = {"Content-Type": "application/json"}
+            response = self.client.post("/resource/mint", json=payload, headers=headers)
+            self.token_dict.append_token(
+                "cow_carcass_token", response.json()["tokenId"]
+            )
 
-        payload = {
-            "from_wallet_address": "0x0b97F7B3FC38bF1DFf740d65B582c61b3E84FfC6",
-            "producer_token_id": response.json()["tokenId"],
-            "metaData": {"key": "value"},
-        }
+        if (
+            len(self.token_dict["sheep_carcass_token"]) > 0
+            and len(self.token_dict["cow_carcass_token"]) > 0
+        ):
+            # transfer sheep
+            payload = {
+                "tokenId": self.token_dict["sheep_carcass_token"][-1],
+                "quantity": 1,
+                "from_wallet_address": self.wallet_addresses["BREEDER"],
+                "to_wallet_address": self.wallet_addresses["SLAUGHTERER"],
+            }
+
+            headers = {"Content-Type": "application/json"}
+            response = self.client.post(
+                "/resource/transfer", json=payload, headers=headers
+            )
+
+            # transfer cow
+            payload = {
+                "tokenId": self.token_dict["cow_carcass_token"][-1],
+                "quantity": 1,
+                "from_wallet_address": self.wallet_addresses["BREEDER"],
+                "to_wallet_address": self.wallet_addresses["SLAUGHTERER"],
+            }
+
+            headers = {"Content-Type": "application/json"}
+            response = self.client.post(
+                "/resource/transfer", json=payload, headers=headers
+            )
+
+        print(self.token_dict.token_dict)
+
+    @task
+    def create_demi_carcasses(self):
+        print("Creating demi carcasses")
+        if (
+            len(self.token_dict["sheep_carcass_token"]) > 0
+            and len(self.token_dict["cow_carcass_token"]) > 0
+        ):
+            # create sheep demi carcass
+            payload = {
+                "from_wallet_address": self.wallet_addresses["SLAUGHTERER"],
+                "producer_token_id": self.token_dict["sheep_carcass_token"].pop(),
+                "metaData": {"key": "value"},
+            }
+
+            headers = {"Content-Type": "application/json"}
+            response = self.client.post(
+                "/resource/mintToMany", json=payload, headers=headers
+            )
+            for response_json in response.json():
+                self.token_dict.append_token(
+                    "sheep_demi_carcass_token", response_json["tokenId"]
+                )
+
+            # create cow demi carcass
+            payload = {
+                "from_wallet_address": self.wallet_addresses["SLAUGHTERER"],
+                "producer_token_id": self.token_dict["cow_carcass_token"].pop(),
+                "metaData": {"key": "value"},
+            }
+
+            headers = {"Content-Type": "application/json"}
+            response = self.client.post(
+                "/resource/mintToMany", json=payload, headers=headers
+            )
+            for response_json in response.json():
+                self.token_dict.append_token(
+                    "cow_demi_carcass_token", response_json["tokenId"]
+                )
+
+        if (
+            len(self.token_dict["sheep_demi_carcass_token"]) > 0
+            and len(self.token_dict["cow_demi_carcass_token"]) > 0
+        ):
+            # transfer sheep demi carcass
+            payload = {
+                "tokenId": self.token_dict["sheep_demi_carcass_token"][-1],
+                "quantity": 1,
+                "from_wallet_address": self.wallet_addresses["SLAUGHTERER"],
+                "to_wallet_address": self.wallet_addresses["MANUFACTURER"],
+            }
+            headers = {"Content-Type": "application/json"}
+            response = self.client.post(
+                "/resource/transfer", json=payload, headers=headers
+            )
+
+            # transfer cow demi carcass
+            payload = {
+                "tokenId": self.token_dict["cow_demi_carcass_token"][-1],
+                "quantity": 1,
+                "from_wallet_address": self.wallet_addresses["SLAUGHTERER"],
+                "to_wallet_address": self.wallet_addresses["MANUFACTURER"],
+            }
+            headers = {"Content-Type": "application/json"}
+            response = self.client.post(
+                "/resource/transfer", json=payload, headers=headers
+            )
+
+        print(self.token_dict.token_dict)
+
+    @task
+    def create_meat(self):
+        print("Creating meat")
+        if (
+            len(self.token_dict["sheep_demi_carcass_token"]) > 0
+            and len(self.token_dict["cow_demi_carcass_token"]) > 0
+        ):
+
+            # create sheep meat
+            payload = {
+                "from_wallet_address": self.wallet_addresses["MANUFACTURER"],
+                "producer_token_id": self.token_dict["sheep_demi_carcass_token"].pop(),
+                "metaData": {"key": "value"},
+            }
+
+            headers = {"Content-Type": "application/json"}
+            response = self.client.post(
+                "/resource/mintToMany", json=payload, headers=headers
+            )
+            for response_json in response.json():
+                self.token_dict.append_token("meat_token", response_json["tokenId"])
+
+            # create cow meat
+            payload = {
+                "from_wallet_address": self.wallet_addresses["MANUFACTURER"],
+                "producer_token_id": self.token_dict["cow_demi_carcass_token"].pop(),
+                "metaData": {"key": "value"},
+            }
+
+            headers = {"Content-Type": "application/json"}
+            response = self.client.post(
+                "/resource/mintToMany", json=payload, headers=headers
+            )
+            for response_json in response.json():
+                self.token_dict.append_token("meat_token", response_json["tokenId"])
+
+        print(self.token_dict.token_dict)
