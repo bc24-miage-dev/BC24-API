0a73f0bd80f1ba0339e6a81fb5942fecc2d32bba
Etienne Baumgartner <baume96@gmail.com>
2024-06-30 14:53:07 +0200

    refacto(main entrypoint): changed text when arriving on the site

--- main.py
+++ main.py
@@ -4,25 +4,28 @@ from starlette.middleware.base import BaseHTTPMiddleware
 
 from api.router import router
 
-app = FastAPI(title=settings.title,
-              version=settings.version,
-              description=settings.description,
-              docs_url=settings.docs_url,
-              redoc_url=settings.redoc_url,
-              openapi_url=settings.openapi_url,)
-
+app = FastAPI(
+    title=settings.title,
+    version=settings.version,
+    description=settings.description,
+    docs_url=settings.docs_url,
+    redoc_url=settings.redoc_url,
+    openapi_url=settings.openapi_url,
+)
 
 
 class NoCacheMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
         response = await call_next(request)
-        response.headers['Cache-Control'] = 'no-store'
+        response.headers["Cache-Control"] = "no-store"
         return response
+
 
 app.add_middleware(NoCacheMiddleware)
 
 app.include_router(router, prefix=settings.api_prefix)
 
+
 @app.get("/")
 async def root():
-    return {"Say": "Hello!"}
+    return {"API is running fine. Please visit /docs for API documentation."}
